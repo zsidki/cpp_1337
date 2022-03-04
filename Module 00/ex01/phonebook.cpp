58f11da78cb679958b717c36e7cc0c49988bfc1f
Zakariya Sidki <zsidki@e1r5p5.1337.ma>
2022-03-04 19:12:28 +0100

    startex01

--- Module 00/ex01/phonebook.cpp
+++ Module 00/ex01/phonebook.cpp
@@ -2,12 +2,36 @@
 class Phonebook{
 
     private:
+	std::string array_contacts[8];
 
     public :
 
 };
 
+class Contact{
 
+    private:
+
+    int index;
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+
+
+
+    public :
+    int set_index();
+    std::string set_first_name();
+    std::string set_last_name();
+    std::string set_nickname();
+
+    int get_index();
+    std::string get_first_name();
+    std::string get_last_name();
+    std::string get_nickname();
+
+
+};
 
 int     main()
 {
