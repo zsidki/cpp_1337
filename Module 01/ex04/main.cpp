a225471f9d37833a2e229a6c6c44fed66a7c320d
Zakariya Sidki <zsidki@e1r2p6.1337.ma>
2022-03-16 18:51:45 +0100

    EX04 M01 DONE

--- Module 01/ex04/main.cpp
+++ Module 01/ex04/main.cpp
@@ -4,17 +4,22 @@
 
 int	main(int argc, char **argv)
 {
+	if(argc != 4 )
+	{
+		std::cout << "Error : Invalid Argument! " << std::endl;
+		return 0;
+	}
 	std::string s1;
 	std::string s2;
-	std::ofstream myfile;
 
 	int	index = 0;
 	s1 = std::string(argv[2]);
 	s2 = std::string(argv[3]);
-	
-	if(argc != 4)
+
+
+	if (s1.empty() || s2.empty())
 	{
-		std::cout << "Error : invalid argument! " << std::endl;
+		std::cout << "Error : Empty String! " << std::endl;
 		return 0;
 	}
 
@@ -27,17 +32,32 @@ int	main(int argc, char **argv)
 		std::ofstream output_file(out_file.c_str());
 		std::string line;
 
-		for (int line_num = 1; std::getline(input_file, line); ++line_num)
+		if(!(input_file.is_open()))
 		{
-			index = 0;
-			while (line.find(s1 ,index) != std::string::npos)
+			std::cout << "Error : The file doesn't exist." << std::endl;
+			return 0;
+		}
+		while (std::getline(input_file, line ))
+		{
+			if (s1 == s2)
+				output_file << line << std::endl;
+			else
 			{
-				index = line.find(s1);
-				line.erase(index,s1.length());
-				line.insert(index,s2);
-				index++;
+				index = 0;
+				while (line.find(s1 ,index) != std::string::npos)
+				{
+			
+					index = line.find(s1);
+					line.erase(index,s1.length());
+					line.insert(index,s2);
+					index++;
+				}
+				if (input_file.eof())
+					output_file << line;
+				else
+					output_file << line << std::endl;
+
 			}
-			output_file << line << std::endl;
 		}
 		if (input_file)
 		{
@@ -46,5 +66,6 @@ int	main(int argc, char **argv)
 		}
 		std::cin.get();
 	}
+
     return 0;
 }
