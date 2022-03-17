35b2681a2cd4edd67015faafc8e9a1279e3b3e0a
Zakariya Sidki <zsidki@e1r2p3.1337.ma>
2022-03-17 21:51:18 +0100

    Starting EX00 M02

---
+++ Module 02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class	    Fixed{
+
+	private:
+
+
+	public:
+    void setRawBits( int const raw );
+    int getRawBits( void ) const;
+
+};
+
+#endif
