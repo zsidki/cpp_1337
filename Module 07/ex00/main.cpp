da861e217dfd0cb8332520b381666b7a3c7357f5
Zakariya Sidki <zsidki@e2r4p1.1337.ma>
2022-04-06 15:07:17 +0000

    ex00 M07 Done

--- Module 07/ex00/main.cpp
+++ Module 07/ex00/main.cpp
@@ -1,14 +1,34 @@
 #include "whatever.hpp"
 
+// class Awesome
+// {
+// public:
+//     Awesome(void) : _n(0) {}
+//     Awesome( int n ) : _n( n ) {}
+//     Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+//     bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+//     bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+//     bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+//     bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+//     bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+//     bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+//     int get_n() const { return _n; }
+// private:
+//     int _n;
+// };
 
-void    swap(int a, int b)
-{
-    int tmp;
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
 
-    tmp = a;
-    a = b;
-    b = tmp;
-}
+// int main(void)
+// {
+//     Awesome a(2), b(4);
+//     swap(a, b);
+
+//     std::cout << a << " " << b << std::endl;
+//     std::cout << max(a, b) << std::endl;
+//     std::cout << min(a, b) << std::endl;
+//     return (0);
+// }
 
 
 
@@ -16,13 +36,13 @@ int main( void )
 {
     int a = 2;
     int b = 3;
-    ::swap( a, b );
+    swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
-    ::swap(c, d);
+    swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
--- Module 07/ex00/whatever.hpp
+++ Module 07/ex00/whatever.hpp
@@ -3,5 +3,32 @@
 
 #include <iostream>
 
+template <typename T>
+void    swap(T a, T b)
+{
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T x, T y)
+{
+    if (x < y)
+        return x;
+    else
+        return y;
+}
+
+template <typename T>
+T max(T x, T y)
+{
+    if (x > y)
+        return x;
+    else
+        return y;
+}
 
 #endif
