aee9e61ccac6dcba32716c8e08e852e66d119c0f
Zakariya Sidki <zsidki@e1r7p2.1337.ma>
2022-04-07 01:27:53 +0000

    EX02 M07 Done

--- Module 07/ex02/Array.hpp
+++ Module 07/ex02/Array.hpp
@@ -5,8 +5,68 @@
 
 template<class T> 
     class Array // template definition
-    { 
-        
+    {
+        private:
+            T *_array;
+            unsigned int _size;
+
+            
+
+        public:
+
+            //----- Constructor & Destructor -------//
+            Array(void) : _array(new T[0]), _size(0) {};
+
+            Array(unsigned int n) : _size(n) 
+            {  
+                this->_array = new T[n];
+            };
+
+            Array(const Array& array) : _array(new T[array._size]), _size(array._size)
+            {
+                *this = array;
+            }
+
+            ~Array()
+            {
+                delete[] this->_array;
+            }
+            
+            //copy contents of array in other array
+            Array& operator=(const Array& array)
+            {
+                for(unsigned int i = 0; i < array._size && i < this->_size; i++)
+                    this->_array[i] = array._array[i];
+                return *this;
+            }
+
+            //subscript operator: [ ]
+            T& operator[](unsigned int index)
+            {
+                if(index >= this->_size)
+                    throw OutOfBoundsException();
+                return (this->_array[index]);
+            }
+            const T& operator[](unsigned int index) const
+            {
+                if(index >= this->_size)
+                    throw OutOfBoundsException();
+                return (this->_array[index]);
+            }
+
+            unsigned int size() const 
+            {
+                return this->_size;
+            }
+
+            class OutOfBoundsException : public std::exception
+            {   public:
+                    virtual const char* what() const  throw()
+                    {
+                        return ("Out Of Bounds Exception");
+                    }
+            };
+
     }; 
 
 #endif
--- Module 07/ex02/main.cpp
+++ Module 07/ex02/main.cpp
@@ -1,54 +1,61 @@
 #include <iostream>
-#include <Array.hpp>
+#include "Array.hpp"
 
-// #define MAX_VAL 750
+int     main()
+{
+    std::cout << "//--- create normal array ---//\n";
+    int * a = new int();
+    std::cout<< "a = " << *a << std::endl;
 
-// int main(int, char**)
-// {
-//     Array<int> numbers(MAX_VAL);
-//     int* mirror = new int[MAX_VAL];
-//     srand(time(NULL));
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         const int value = rand();
-//         numbers[i] = value;
-//         mirror[i] = value;
-//     }
-//     //SCOPE
-//     {
-//         Array<int> tmp = numbers;
-//         Array<int> test(tmp);
-//     }
 
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         if (mirror[i] != numbers[i])
-//         {
-//             std::cerr << "didn't save the same value!!" << std::endl;
-//             return 1;
-//         }
-//     }
-//     try
-//     {
-//         numbers[-2] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-//     try
-//     {
-//         numbers[MAX_VAL] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
+    std::cout << "//--- create new type array ---//\n";
+    Array<int> a1(4);
+    std::cout << "SIZE of a1 == " << a1.size() << std::endl;
 
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         numbers[i] = rand();
-//     }
-//     delete [] mirror;//
-//     return 0;
-// }
+    for(unsigned int i = 0; i < a1.size(); i++)
+        a1[i] = i;
+
+    std::cout<<"\n// -------- Print Values a1 ---------- //\n";
+    for(unsigned int i = 0; i < a1.size(); i++)
+        std::cout<< "a1 = " << a1[i] << std::endl;
+
+
+    //------ a1 = a2 -----//
+    Array<int> a2(a1);
+
+    std::cout<<"\n// -------- Print Values a2 ---------- //\n";
+    for(unsigned int i = 0; i < a2.size(); i++)
+        std::cout<< "a2 = " << a2[i] << std::endl;
+
+
+    std::cout<<"\n// -------- size a2 > a1 ---------- //\n";
+    try
+    {
+        a2[10] = 84;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+
+    }
+
+    std::cout<<"\n// ------ empty array -------- //\n";
+    
+    Array<int> empty;
+    try
+    {
+        empty[0] = 1;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    std::cout<<"\n// ------ const array -------- //\n";
+    const Array<int> ax(3);
+    //ax[0] = 10;
+    std::cout << "size const array = " << ax.size() << std::endl;
+
+
+    return 0;
+}
